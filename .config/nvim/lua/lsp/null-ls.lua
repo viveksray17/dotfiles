dbc9c4dbe07cab6c2bf21ac2dd9ca5489a42db71
Vivek Sundaray <viveksundaray123@outlook.com>
2022-01-31 08:06:43 +0530

    done some formatting changes

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -12,10 +12,10 @@ null_ls.setup({
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
 			vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
+			augroup LspFormatting
+				autocmd! * <buffer>
+				autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+			augroup END
             ]])
 		end
 	end,
