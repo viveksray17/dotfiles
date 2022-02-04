9f1f1ae86c297a8761cebcf9e8635f1e69310f54
Vivek Sundaray <viveksundaray123@outlook.com>
2022-02-04 16:57:52 +0530

    added configuration for netrw

---
+++ .config/nvim/lua/user/netrw.lua
@@ -0,0 +1,9 @@
+local options = {
+  netrw_banner = 0,
+  netrw_localcopydircmd = "cp -r",
+  netrw_fastbrowse = 0,
+  netrw_list_hide = "__pycache__,__init__.py,node_modules,build",
+}
+for k, v in pairs(options) do
+  vim.g[k] = v
+end
