9644790d13ef6112cbad1e534ba587fbc61b52a2
Vivek Sundaray <viveksundaray123@outlook.com>
2022-02-07 12:38:46 +0530

    added some exception

--- .config/nvim/lua/user/netrw.lua
+++ .config/nvim/lua/user/netrw.lua
@@ -2,7 +2,7 @@ local options = {
   netrw_banner = 0,
   netrw_localcopydircmd = "cp -r",
   netrw_fastbrowse = 0,
-  netrw_list_hide = "__pycache__,__init__.py,.git,node_modules,build",
+  netrw_list_hide = "__pycache__,__init__.py,.git,node_modules,build,venv",
 }
 for k, v in pairs(options) do
   vim.g[k] = v
