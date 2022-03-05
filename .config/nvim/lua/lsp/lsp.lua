8537ca394dadd2ff10b52243f3c719598bfa66ba
Vivek Sundaray <viveksundaray123@outlook.com>
2022-03-05 12:42:23 +0530

    added a setting for python to turn off typechecking

--- .config/nvim/lua/lsp/lsp.lua
+++ .config/nvim/lua/lsp/lsp.lua
@@ -1,3 +1,10 @@
 require("lspconfig").pyright.setup({
   on_attach = require("lsp.handlers").on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off",
+      },
+    },
+  },
 })
