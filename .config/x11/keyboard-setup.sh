7fa7e8618b457a00049100b22b47c293bba5fc6c
Vivek Sundaray <viveksundaray123@outlook.com>
2022-04-04 07:27:24 +0530

    move the non-xorg commands to a separate script

---
+++ .config/x11/keyboard-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+setxkbmap -option ctrl:nocaps
+xcape -e 'Control_L=Escape'
+xmodmap -e 'keycode 37 = '
+xmodmap -e 'keycode 108 = Caps_Lock'
---
+++ .config/x11/rclone-gdrive-mount.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ping -c 1 -q google.com >&/dev/null && rclone mount --daemon google-drive: /home/vivek/Documents/GoogleDrive/ &
