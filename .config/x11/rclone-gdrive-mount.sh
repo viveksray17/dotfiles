c05ae1a28b1bb0729a4615905a9a23f998b26e51
Vivek Sundaray <viveksundaray123@outlook.com>
2022-07-13 12:22:39 +0530

    changed the mount script a bit

--- .config/x11/rclone-gdrive-mount.sh
+++ .config/x11/rclone-gdrive-mount.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ping -c 1 -q google.com >&/dev/null && rclone mount --daemon google-drive: /home/vivek/Documents/GoogleDrive/ && notify-send "rclone" "Google Drive Mounted"&
+ping -c 1 -q google.com >&/dev/null && rclone mount --daemon google-drive: /home/vivek/Documents/GoogleDrive/ && notify-send "rclone" "Google Drive Mounted" || notify-send "rclone" "Unable to Mount\nNo internet connection"
