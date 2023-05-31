5147610fa04b154ec37a640d5ea27b0631a19a04
Vivek Sundaray <viveksundaray123@outlook.com>
2023-05-31 11:44:16 +0530

    did some changes long ago

--- .config/x11/device-setup.sh
+++ .config/x11/device-setup.sh
@@ -3,5 +3,4 @@ setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
 xmodmap -e 'keycode 37 = '
 xmodmap -e 'keycode 108 = Caps_Lock'
-xinput disable "SynPS/2 Synaptics TouchPad" # disable the trackpad
 xinput set-prop "PixArt Lenovo USB Optical Mouse" "libinput Accel Speed" -0.7 # slow the sleep of the mouse
